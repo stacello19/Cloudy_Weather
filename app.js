7bbfbbbbf726f723d05e7e650f1e0acafc89e06b
Stacy Cho <stacello19@gmail.com>
2019-03-21 12:38:03 -0400

    port update

--- app.js
+++ app.js
@@ -44,4 +44,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(process.env.PORT || '3000', function(){
+  console.log('I am listening %d %s', this.address().port, app.settings.env);
+})
+
 module.exports = app;
